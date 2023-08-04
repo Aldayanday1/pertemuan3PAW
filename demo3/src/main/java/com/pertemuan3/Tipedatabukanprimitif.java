dc6e2def850161e9a5a472e9d0ad5429a0c371ae
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-04 07:49:09 +0700

    Tipe Data Bukan Primitif

---
+++ demo3/src/main/java/com/pertemuan3/Tipedatabukanprimitif.java
@@ -0,0 +1,12 @@
+package com.pertemuan3;
+
+public class Tipedatabukanprimitif {
+    public static void main(String[] args) {
+        Integer nilai = 10; 
+        Character huruf = 'A'; 
+        Boolean status = true; 
+        System.out.println("Nilai: " + nilai);
+        System.out.println("Huruf: " + huruf);
+        System.out.println("Status: " + status);
+    }
+}
