26c0643e22c1fc4c090170e83dfe4a15e1795fa3
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-04 07:49:58 +0700

    Tipe Data Array

---
+++ demo3/src/main/java/com/pertemuan3/Tipedataarray.java
@@ -0,0 +1,13 @@
+package com.pertemuan3;
+
+public class Tipedataarray {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5}; 
+        String[] buah = new String[3];
+        buah[0] = "Apel";
+        buah[1] = "Pisang";
+        buah[2] = "Jeruk";
+        System.out.println("Angka[2]: " + angka[2]);
+        System.out.println("Buah[1]: " + buah[1]);
+    }
+}
