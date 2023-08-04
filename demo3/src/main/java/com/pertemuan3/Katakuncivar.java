f5d0d65bec4087fb8a371464f6519fbc4d7c6298
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-04 07:46:56 +0700

    Kata Kunci Var

---
+++ demo3/src/main/java/com/pertemuan3/Katakuncivar.java
@@ -0,0 +1,11 @@
+package com.pertemuan3;
+
+public class Katakuncivar {
+    public static void main(String[] args) {
+        int usia = 20;
+        String nama = "Aldi";
+        System.out.println("Nama: " + nama);
+        System.out.println("Usia: " + usia);
+    }
+    
+}
