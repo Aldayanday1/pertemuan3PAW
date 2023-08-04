65b396077f3193b98ef623c71b187f4a21702c91
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-04 07:53:03 +0700

    Augmented Assignments

---
+++ demo3/src/main/java/com/pertemuan3/Augmentedassignments.java
@@ -0,0 +1,13 @@
+package com.pertemuan3;
+
+public class Augmentedassignments {
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 5;
+        angka1 += angka2; // Sama dengan angka1 = angka1 + angka2;
+        System.out.println("angka1: " + angka1);
+        double nilai = 15.5;
+        nilai *= 2; // Sama dengan nilai = nilai * 2;
+        System.out.println("nilai: " + nilai);
+    }
+}
