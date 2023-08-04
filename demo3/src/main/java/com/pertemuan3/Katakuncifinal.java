4c8f452752fb316e8cb9743a0548a61485dbd55b
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-04 07:48:04 +0700

    Kata Kunci Final

---
+++ demo3/src/main/java/com/pertemuan3/Katakuncifinal.java
@@ -0,0 +1,8 @@
+package com.pertemuan3;
+
+public class Katakuncifinal {
+    public static void main(String[] args) {
+        final int maksimalPercobaan = 3;
+        System.out.println("Maksimal Percobaan: " + maksimalPercobaan);
+    }
+}
