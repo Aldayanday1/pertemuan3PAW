35c5acfe3bac8401647d5a2d12d790a0211f6756
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-04 07:41:23 +0700

    Variable

---
+++ demo3/src/main/java/com/pertemuan3/Variable.java
@@ -0,0 +1,14 @@
+package com.pertemuan3;
+
+public class Variable {
+    public static void main(String[] args) {
+        int usia = 20; 
+        double gaji = 5000000; 
+        boolean sudahMenikah = false; 
+        String nama = "Aldi"; 
+        System.out.println("Nama: " + nama);
+        System.out.println("Usia: " + usia);
+        System.out.println("Gaji: " + gaji);
+        System.out.println("Sudah Menikah: " + sudahMenikah);
+    }
+}
