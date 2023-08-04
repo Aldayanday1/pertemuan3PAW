dca72cf310ca1ddb8284a8a806150678a2b5b341
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-04 07:51:30 +0700

    Operasi Matematika

---
+++ demo3/src/main/java/com/pertemuan3/Operasimatematika.java
@@ -0,0 +1,19 @@
+package com.pertemuan3;
+
+public class Operasimatematika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int total = a + b;
+        int selisih = a - b;
+        int hasilKali = a * b;
+        int hasilBagi = a / b;
+        int sisaBagi = a % b;
+
+        System.out.println("Total: " + total);
+        System.out.println("Selisih: " + selisih);
+        System.out.println("Hasil Kali: " + hasilKali);
+        System.out.println("Hasil Bagi: " + hasilBagi);
+        System.out.println("Sisa Bagi: " + sisaBagi);
+    }
+}
